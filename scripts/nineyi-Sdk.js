8037bf59231f23669ec43cac55c82f429d81a18b
jerrytang <jerrytang@nine-yi.com>
2017-01-11 11:02:37 +0800

    修正ts版

--- scripts/nineyi-Sdk.js
+++ scripts/nineyi-Sdk.js
@@ -8,6 +8,7 @@ var NineYi;
             (function (SDK) {
                 var DownloadAppBanner = (function () {
                     function DownloadAppBanner() {
+                        var _this = this;
                         this.Version = "1.0.0";
                         this.Domain = "http://shop.s3.com.tw/";
                         this.ShopIconUrl = "https://diz36nn4q02zr.cloudfront.net/webapi/images/t/512/512/ShopIcon/156/0/211027";
@@ -23,45 +24,23 @@ var NineYi;
                             this.LoadFontawesome();
                             this.AddpendBanner();
                             if (this.IsShow) {
-                                var smartAppBannerElem = document.getElementById("smart-app-banner");
-                                this.FixHeaderMobileStyle(true);
-                                var _this = this;
-                                if (smartAppBannerElem) {
-                                    window.addEventListener('scroll', function () {
-                                        var scrollY = window.scrollY;
-                                        console.log("scrollY:",scrollY);
-                                        if (scrollY > 130) {
-                                            _this.FixHeaderMobileStyle(false);
-                                        } else {
-                                            _this.FixHeaderMobileStyle(true);
-                                        }
-                                    });
-                                }
+                                this.ModifyHeaderMobileStyle(false);
+                                window.addEventListener('scroll', function () {
+                                    var scrollY = window.scrollY;
+                                    console.log("scrollY:", scrollY);
+                                    if (scrollY > 130) {
+                                        _this.ModifyHeaderMobileStyle(true);
+                                    }
+                                    else {
+                                        _this.ModifyHeaderMobileStyle(false);
+                                    }
+                                });
                             }
                             else {
                                 document.getElementById("smart-app-banner").style.display = "none";
                             }
                         }
                     }
-                    DownloadAppBanner.prototype.FixHeaderMobileStyle = function (status) {
-                        var header_mobile_top = document.getElementById("header_mobile_top");
-                        var header_mobile_bottom = document.getElementById("header_mobile_bottom");
-                        if(status){
-                            if(header_mobile_top){
-                                header_mobile_top.style.position = "relative";
-                            }
-                            if(header_mobile_bottom){
-                                header_mobile_bottom.style.marginTop = "0";
-                            }
-                        } else {
-                            if(header_mobile_top){
-                                header_mobile_top.style.position = "fixed";
-                            }
-                            if(header_mobile_bottom){
-                                header_mobile_bottom.style.marginTop = "130";
-                            }
-                        }
-                    };
                     DownloadAppBanner.prototype.GetIsMobileBrowser = function () {
                         var check = false;
                         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
@@ -77,30 +56,48 @@ var NineYi;
                         newCss.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
                         headID.appendChild(newCss);
                     };
+                    DownloadAppBanner.prototype.ModifyHeaderMobileStyle = function (fixed) {
+                        var header_mobile_top = document.getElementById("header_mobile_top");
+                        var header_mobile_bottom = document.getElementById("header_mobile_bottom");
+                        if (fixed) {
+                            if (header_mobile_top) {
+                                header_mobile_top.style.position = "fixed";
+                            }
+                            if (header_mobile_bottom) {
+                                header_mobile_bottom.style.marginTop = "130";
+                            }
+                        }
+                        else {
+                            if (header_mobile_top) {
+                                header_mobile_top.style.position = "relative";
+                            }
+                            if (header_mobile_bottom) {
+                                header_mobile_bottom.style.marginTop = "0";
+                            }
+                        }
+                    };
                     DownloadAppBanner.prototype.AddpendBanner = function () {
                         var smart_app_banner = document.createElement('div');
                         smart_app_banner.id = "smart-app-banner";
-                        var bannerHtml = '<div class="smart-app-banner" style="background-color:#f8f8f8;color:#595959;position:static;border-bottom:1px solid #dfdfdf;height:130px;">'+
-                            '<div class="banner-container" style="margin:0 auto;padding:35px;">'+
-                            '<div class="banner-content" style="cursor:pointer;position:relative;font-family:PingFang-SC,sans-serif;">'+
-                            '<a class="banner-close-btn" href="javascript: downloadApp.CloseBanner()" style="font-size:30px;color:#595959;margin-right:40px;"><i class="fa fa-times fa-2x" aria-hidden="true"></i></a>'+
-                            '<img class="banner-shop-icon" src="https://diz36nn4q02zr.cloudfront.net/webapi/images/t/512/512/ShopIcon/156/0/211027" style="display:inline-block;vertical-align:middle;border-radius:7px;width:100px;height:100px;margin-top:-30px;">'+
-                            '<span class="banner-body" style="display:inline-block;vertical-align:middle;padding:0 40px 25px;font-size:50px;width:55%;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;">'+
-                            '<a class="banner-name" target="_blank" href="javascript: downloadApp.Download()" style="display:block;font-family:PingFang-SC,sans-serif;font-size:35px;color:#595959;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;text-decoration:none">'+this.AppName+'</a>'+
-                            '<a class="banner-desc" style="display:block;font-size:35px;color:#b7b7b7;text-decoration:none">立刻使用官方APP</a>'+
-                            '</span>'+
-                            '<div class="banner-download-btn" style="width:160px;height:75px;line-height:70px;border-radius:3px;border:solid 1px #595959;position:absolute;right:0;top:2px;text-align:center;">'+
-                            '<a class="banner-download-btn-text" href="javascript: downloadApp.Download()" style="color:#595959;font-family:PingFang-SC,sans-serif;font-size:40px;text-decoration:none">下載</a>'+
-                            '</div>'+
-                            '</div>'+
-                            '</div>'+
+                        smart_app_banner.style.position = "relative";
+                        var bannerHtml = '<div class="smart-app-banner" style="background-color:#f8f8f8;color:#595959;position:static;border-bottom:1px solid #dfdfdf;height:130px;">' +
+                            '<div class="banner-container" style="margin:0 auto;padding:35px;">' +
+                            '<div class="banner-content" style="cursor:pointer;position:relative;font-family:PingFang-SC,sans-serif;">' +
+                            '<a class="banner-close-btn" href="javascript: downloadApp.CloseBanner()" style="font-size:30px;color:#595959;margin-right:40px;"><i class="fa fa-times fa-2x" aria-hidden="true"></i></a>' +
+                            '<img class="banner-shop-icon" src="https://diz36nn4q02zr.cloudfront.net/webapi/images/t/512/512/ShopIcon/156/0/211027" style="display:inline-block;vertical-align:middle;border-radius:7px;width:100px;height:100px;margin-top:-30px;">' +
+                            '<span class="banner-body" style="display:inline-block;vertical-align:middle;padding:0 40px 25px;font-size:50px;width:55%;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;">' +
+                            '<a class="banner-name" target="_blank" href="javascript: downloadApp.Download()" style="display:block;font-family:PingFang-SC,sans-serif;font-size:35px;color:#595959;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;text-decoration:none">' + this.AppName + '</a>' +
+                            '<a class="banner-desc" style="display:block;font-size:35px;color:#b7b7b7;text-decoration:none">立刻使用官方APP</a>' +
+                            '</span>' +
+                            '<div class="banner-download-btn" style="width:160px;height:75px;line-height:70px;border-radius:3px;border:solid 1px #595959;position:absolute;right:0;top:2px;text-align:center;">' +
+                            '<a class="banner-download-btn-text" href="javascript: downloadApp.Download()" style="color:#595959;font-family:PingFang-SC,sans-serif;font-size:40px;text-decoration:none">下載</a>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
                             '</div>';
                         smart_app_banner.innerHTML = bannerHtml;
-                        smart_app_banner.style.position = "relative";
-                        var header_mobile = document.getElementById("header_mobile_top");
                         if (document.body) {
                             document.body.insertBefore(smart_app_banner, document.body.childNodes[0]);
-                            console.log("document.body:", document.body.childNodes[0]);
                         }
                     };
                     DownloadAppBanner.prototype.CloseBanner = function () {
@@ -113,7 +110,6 @@ var NineYi;
                     };
                     return DownloadAppBanner;
                 }());
-                SDK.DownloadAppBanner = DownloadAppBanner;
             })(SDK = FrontEnd.SDK || (FrontEnd.SDK = {}));
         })(FrontEnd = Mall.FrontEnd || (Mall.FrontEnd = {}));
     })(Mall = NineYi.Mall || (NineYi.Mall = {}));

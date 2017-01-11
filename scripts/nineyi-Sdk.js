ea79e03733028a029cfa766c861389ac63fe55ab
jerrytang <jerrytang@nine-yi.com>
2017-01-11 12:37:00 +0800

    刪除appendBanner

--- scripts/nineyi-Sdk.js
+++ scripts/nineyi-Sdk.js
@@ -73,30 +73,6 @@ var NineYi;
                             }
                         }
                     };
-                    DownloadAppBanner.prototype.AddpendBanner = function () {
-                        var smart_app_banner = document.createElement('div');
-                        smart_app_banner.id = "smart-app-banner";
-                        smart_app_banner.style.position = "relative";
-                        var bannerHtml = '<div class="smart-app-banner" style="background-color:#f8f8f8;color:#595959;position:static;border-bottom:1px solid #dfdfdf;height:130px;">' +
-                            '<div class="banner-container" style="margin:0 auto;padding:35px;">' +
-                            '<div class="banner-content" style="cursor:pointer;position:relative;font-family:PingFang-SC,sans-serif;">' +
-                            '<a class="banner-close-btn" href="javascript: downloadApp.CloseBanner()" style="font-size:30px;color:#595959;margin-right:40px;"><i class="fa fa-times fa-2x" aria-hidden="true"></i></a>' +
-                            '<img class="banner-shop-icon" src="https://diz36nn4q02zr.cloudfront.net/webapi/images/t/512/512/ShopIcon/156/0/211027" style="display:inline-block;vertical-align:middle;border-radius:7px;width:100px;height:100px;margin-top:-30px;">' +
-                            '<span class="banner-body" style="display:inline-block;vertical-align:middle;padding:0 40px 25px;font-size:50px;width:55%;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;">' +
-                            '<a class="banner-name" target="_blank" href="javascript: downloadApp.Download()" style="display:block;font-family:PingFang-SC,sans-serif;font-size:35px;color:#595959;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;text-decoration:none">' + this.AppName + '</a>' +
-                            '<a class="banner-desc" style="display:block;font-size:35px;color:#b7b7b7;text-decoration:none">立刻使用官方APP</a>' +
-                            '</span>' +
-                            '<div class="banner-download-btn" style="width:160px;height:75px;line-height:70px;border-radius:3px;border:solid 1px #595959;position:absolute;right:0;top:2px;text-align:center;">' +
-                            '<a class="banner-download-btn-text" href="javascript: downloadApp.Download()" style="color:#595959;font-family:PingFang-SC,sans-serif;font-size:40px;text-decoration:none">下載</a>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>';
-                        smart_app_banner.innerHTML = bannerHtml;
-                        if (document.body) {
-                            document.body.insertBefore(smart_app_banner, document.body.childNodes[0]);
-                        }
-                    };
                     DownloadAppBanner.prototype.CloseBanner = function () {
                         this.IsShow = false;
                         document.getElementById("smart-app-banner").style.display = "none";
